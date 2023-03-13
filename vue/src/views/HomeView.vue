3dc12fd66a07fd26a4d8af6aa5ae1e0a08617180
cassidyrobles <cassidyrobles19@gmail.com>
2023-03-13 13:15:42 -0400

    nah

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,23 +1,45 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <main>
+    <TheWelcome/>
+  </main>
+
+  <div class="home">
+    <h1>{{ student }}</h1>
+  </div>
+
+</template>
+
+<script>
+
+export default{
+name: "Home",
+components: {},
+data(){
+  return {
+  student: "Harry",
+  graduated: true,
+  };
+},
+};
+
 </script>
 <style>
 h1{
   color:aqua
 }
 </style>
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-  <div id="app">
-    <h1>hello</h1>
-  </div>
-</template>
+
+
 <!--shipping html file to load javascript so html and javacript are done with javascript-->
 <div id="app"></div>
 <!--all  javascript is injected into div-->
 <!--app.view is where we add all of our routes like home and view-->
 <!--script section is where we name components, declare variables, put functions, name data that will be updated or called in html
 we can also do css in styles
-we use npm run serve instead of dev now-->
+you can make a new page in route in app.vue
+router view is the page you are currently on
+the contents of router view are whatever page you are currently selecting
+scoped syling prevents the styling of a folder or app moving to other components
+{{student}} takes student from the array and uses it as an h1
+script is where js goes
+template is where html goes-->
